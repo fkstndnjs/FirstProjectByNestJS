55907ea673674b5b46ca2f2ab696fd287eb9294f
YuSeokHyun <fkstndnjs@naver.com>
2022-12-26 11:43:29 +0900

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://fkstndnjs:<password>@nest.m8wgg3n.mongodb.net/test',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
