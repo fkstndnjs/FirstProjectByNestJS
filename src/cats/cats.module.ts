b332cafe680035c199f0334864139a1d16884f28
YuSeokHyun <fkstndnjs@naver.com>
2022-12-26 12:28:35 +0900

    fix

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule.forRoot()],
   controllers: [CatsController],
-  providers: [CatsService]
+  providers: [CatsService],
 })
 export class CatsModule {}
