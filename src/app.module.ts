d931c8a9a43e2f8aa6ecfd0af1dd4ea93b238ab2
YuSeokHyun <fkstndnjs@naver.com>
2022-12-26 11:59:28 +0900

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,12 @@ import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URI),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
