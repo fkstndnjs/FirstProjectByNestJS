555bc8099dfe89bba5e6a2bc79cf48ab915f2e07
YuSeokHyun <fkstndnjs@naver.com>
2022-12-26 11:50:56 +0900

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,6 +9,7 @@ import { AppService } from './app.service';
     MongooseModule.forRoot(
       'mongodb+srv://fkstndnjs:dbtjrgus7@@nest.m8wgg3n.mongodb.net/cats?retryWrites=true&w=majority',
     ),
+    ConfigModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
