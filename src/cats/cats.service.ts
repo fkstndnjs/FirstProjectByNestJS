e2754b946ab952686b08698e7198965152771e39
YuSeokHyun <fkstndnjs@naver.com>
2022-12-26 14:52:46 +0900

    fix

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Cat } from './cats.schema';
@@ -9,9 +9,16 @@ import { UpdateCatDto } from './dto/update-cat.dto';
 export class CatsService {
   constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
 
-  signup(Body: CreateCatDto) {
+  async signup(body: CreateCatDto) {
     const { name, password, email } = body;
 
+    const isCatExist = await this.catModel.exists({
+      email,
+    });
+
+    if (isCatExist) {
+      throw new HttpException('이미 존재하는 계정입니다.', 409);
+    }
     return 'This action adds a new cat';
   }
 
