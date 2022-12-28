58f43ba2de55bb750991e90928c59ce2a1cf9b70
YuSeokHyun <fkstndnjs@naver.com>
2022-12-28 10:07:55 +0900

    fix

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -5,31 +5,32 @@ import { Cat } from './cats.schema';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
 import * as bcrypt from 'bcrypt';
+import { CatsRepository } from './cats.repository';
 
 @Injectable()
 export class CatsService {
-  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+  constructor(private readonly catsRepository: CatsRepository) {}
 
   async signup(body: CreateCatDto) {
     const { name, password, email } = body;
 
-    const isCatExist = await this.catModel.exists({
-      email,
-    });
+    // const isCatExist = await this.catModel.exists({
+    //   email,
+    // });
 
-    if (isCatExist) {
-      throw new HttpException('이미 존재하는 계정입니다.', 409);
-    }
+    // if (isCatExist) {
+    //   throw new HttpException('이미 존재하는 계정입니다.', 409);
+    // }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    // const hashedPassword = await bcrypt.hash(password, 10);
 
-    const cat: any = await this.catModel.create({
-      name,
-      password: hashedPassword,
-      email,
-    });
+    // const cat: any = await this.catModel.create({
+    //   name,
+    //   password: hashedPassword,
+    //   email,
+    // });
 
-    return { ...cat._doc, password: '***' };
+    // return { ...cat._doc, password: '***' };
   }
 
   findAll() {
