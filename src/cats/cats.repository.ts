e8afb78042faa4d035459531a9d798ad5f71b851
YuSeokHyun <fkstndnjs@naver.com>
2022-12-28 09:47:19 +0900

    fix

--- src/cats/cats.repository.ts
+++ src/cats/cats.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Cat } from './cats.schema';
@@ -6,4 +6,14 @@ import { Cat } from './cats.schema';
 @Injectable()
 export class CatsRepository {
   constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  async checkExistsByEmail(email: string): Promise<boolean> {
+    try {
+      const result = await this.catModel.exists({
+        email,
+      });
+    } catch (err) {
+      throw new HttpException(err, 500);
+    }
+  }
 }
